95a702368ae4dba9b9c4c2c8e462b4fb04a9a4a6
Dylan Erickson <Dylanserickson@gmail.com>
2017-02-13 19:01:09 -0800

    adding a projectile set up to be able to enstanciate projectiles when the trigger is pulled

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -8,6 +8,7 @@
 class UTankAimingComponent;
 class UTankBarrel;
 class UTankTurret;
+class AProjectile;
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -41,4 +42,7 @@ private:
 	UPROPERTY(EditAnywhere, Category = Firing)
 	float LaunchSpeed = 4000; // TODO Find sensable default speed
 	
+	UPROPERTY(EditAnywhere, Category = Setup)
+	TSubclassOf<AProjectile> ProjectileBlueprint;
+
 };
